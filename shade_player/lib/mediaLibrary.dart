df0746de479f4ac9c1a1ee7d93bb0ad7733a7335
daliederman <daliederman@gmail.com>
2024-12-28 21:30:27 -0800

    Add support for media metadata
    Courtesy of flutter_media_metadata.

--- shade_player/lib/mediaLibrary.dart
+++ shade_player/lib/mediaLibrary.dart
@@ -4,11 +4,11 @@
 // The library can be filtered to sublists in certain circumstances, such as when searching.
 // These sublists are stored separately.
 
+import 'dart:io';
 import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:sqflite_common/sqlite_api.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:flutter_media_metadata/flutter_media_metadata.dart';
 
 class Library extends ChangeNotifier {
   // Generic functions and comments helpfully provided by Supermaven
@@ -20,6 +20,7 @@ class Library extends ChangeNotifier {
   List<Media> searchList = [];
 
   void addMedia(Media media) async {
+    media.populateMetadata();
     if (mediaList.contains(media)) {
       return;
     }
@@ -30,7 +31,7 @@ class Library extends ChangeNotifier {
       await db.close();
       return;
     }
-    Map<String, String> mediaMap = {
+    Map<String, dynamic> mediaMap = {
       'shuffle': media.shuffle,
       'title': media.title,
       'artist': media.artist,
@@ -39,6 +40,7 @@ class Library extends ChangeNotifier {
       'year': media.year,
       'duration': media.duration,
       'path': media.path,
+      'playCount': media.playCount,
     };
     db.insert('media', mediaMap);
     await db.close();
@@ -62,12 +64,12 @@ class Library extends ChangeNotifier {
     notifyListeners();
   }
 
-  // Load index file and parse it into a list of media objects
-  Future loadIndexFile() async {
+  // Load library database and parse it into a list of media objects
+  Future loadLibraryFile() async {
     var databaseFactory = databaseFactoryFfi; // Potential: Add logic for handling other platforms
     var db = await databaseFactory.openDatabase(dbPath);
 
-    await db.execute('CREATE TABLE IF NOT EXISTS media (id INTEGER PRIMARY KEY, shuffle TEXT, title TEXT, artist TEXT, album TEXT, genre TEXT, year TEXT, duration TEXT, path TEXT)');
+    await db.execute('CREATE TABLE IF NOT EXISTS media (id INTEGER PRIMARY KEY, shuffle TEXT, title TEXT, artist TEXT, album TEXT, genre TEXT, year TEXT, duration TEXT, path TEXT, INTEGER playCount)');
     var entries = await db.query('media');
     for (var entry in entries) {
       mediaList.add(Media(entry['shuffle'].toString(), entry['title'].toString(), entry['artist'].toString(), entry['album'].toString(),
@@ -104,19 +106,35 @@ class Library extends ChangeNotifier {
 }
 
 class Media extends ChangeNotifier {
-  String shuffle;
-  String title;
-  String artist;
-  String album;
-  String genre;
-  String year;
-  String duration;
-  String path;
+  String shuffle = 'true';
+  String title = "";
+  String artist = "";
+  String album = "";
+  String genre = "";
+  String year = "";
+  String duration = "";
+  String path = "";
   // Potential: Support manipulating album art
   bool isPlaying;
+  int playCount = 0;
 
   Media(this.shuffle,this.title, this.artist, this.album, this.genre, this.year, this.duration, this.path, this.isPlaying);
 
+  void populateMetadata() async {
+    final metadata = await MetadataRetriever.fromFile(File(path));
+    if (metadata.trackName != null) title = metadata.trackName!;
+    if (metadata.trackArtistNames != null && metadata.trackArtistNames!.length > 1) {
+      artist = metadata.trackArtistNames!.join(', ');
+    } else if (metadata.trackArtistNames != null && metadata.trackArtistNames!.length == 1) {
+      artist = metadata.trackArtistNames!.first;
+    } else if (metadata.albumArtistName != null) {
+      artist = metadata.albumArtistName!;
+    }
+    if (metadata.albumName != null) album = metadata.albumName!;
+    if (metadata.genre != null) genre = metadata.genre!;
+    if (metadata.year != null) year = metadata.year!.toString();
+    if (metadata.trackDuration != null) duration = metadata.trackDuration!.toString();
+  }
   void setShuffle(String shuffle) {
     if (shuffle.toLowerCase() != 'true' && shuffle.toLowerCase() != 'false') {
       throw Exception('Shuffle must be true or false');
